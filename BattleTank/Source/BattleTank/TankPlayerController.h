3e105ac899b4ed97dd9051de18285b7e47c0cdf9
FIERCEALIEN-PC\FierceAlien <mdcrane133@hotmail.com>
2018-07-15 04:55:44 -0500

    Lecture 158 - EditAnywhere vs EditDefaultsOnly

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -26,13 +26,13 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 		float CrosshairXLocation = 0.5f;
 	
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 		float CrosshairYLocation = 0.3333f;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 		float LineTraceRange = 1000000.0f;
 	
 	ATank* GetControlledTank() const;
